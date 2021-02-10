5c79e04a4696da443b3016fc73394b823ac01155
RyanNickens <22rnickens@athenian.org>
2021-02-10 02:45:16 +0000

    merge sort is fully functional, insertion sort has a bug in it

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,18 +5,71 @@ public class MyMain {
     // Sorts the ArrayList using insertion sort
     public static ArrayList<Integer> insertionSort(ArrayList<Integer> list) {
         // YOUR CODE HERE
-        return null;
+        ArrayList<Integer> sorted = new ArrayList<Integer>(); 
+        //for loop to get and compare indexes
+        for (int i = 0; i < list.size(); i++) {
+            int atm = list.indexOf(i);
+            int j = i;
+            while((j > 0) && (list.indexOf(j - 1) > atm)) {
+                list.set(j, list.indexOf(j - 1));
+                j--;
+            }
+            //setting sorted array value of atm @ j index
+            sorted.set(j, atm);
+        }
+        return sorted;
     }
 
      // Merges two sorted arrays into one large combined
      // sorted array
      // You may assume arr1 and arr2 are the same length
     public static int[] merge(int[] arr1, int[] arr2) { 
-        // YOUR CODE HERE
-        return null;
+        //empty array for values to be added into
+        int[] sorted = new int[arr1.length + arr2.length];
+        //setting variables
+        int first = 0;
+        int second = 0;
+        int third = 0;
+        //code for multiple cases
+        //tells where to put values once compared
+        while (first < arr1.length && second < arr2.length) {
+            if (arr1[first] < arr2[second]) {
+                sorted[third] = arr1[first];
+                first++;
+                third++;
+            } 
+            else {
+                sorted[third] = arr2[second];
+                second++;
+                third++;
+            }
+        }         
+        while (first < arr1.length) {
+            sorted[third] = arr1[first];
+            first++;
+            third++;
+        }          
+        while (second < arr2.length) {
+            sorted[third] = arr2[second];
+            second++;
+            third++;
+        }       
+        return sorted;
     }
 
     public static void main(String[] args) {
         // You can test your code here
-    }
+        ArrayList<Integer> list = new ArrayList<Integer>(); 
+        list.add(2);
+        list.add(4);
+        list.add(3);
+        list.add(6);
+        list.add(8);
+        list.add(9);
+        list.add(7);
+        //System.out.println(insertionSort(list));
+        int[] arr1 = new int[]{ 1,3,5,7,9 }; 
+        int[] arr2 = new int[]{ 2,4,6,8,10 }; 
+        //System.out.println(merge(arr1, arr2));
+        }
 }
